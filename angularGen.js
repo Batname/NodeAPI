b1cd4e60e7e24baf993db9b919abdc0386a72fbd
Batname <dadubinin@gmail.com>
2014-12-28 18:40:36 +0200

    Initial commit

---
+++ angularGen.js
@@ -0,0 +1,41 @@
+var log                 = require('./libs/log')(module);
+var mongoose            = require('./libs/mongoose').mongoose;
+var UserModel           = require('./libs/mongoose').UserModel;
+var ClientModel         = require('./libs/mongoose').ClientModel;
+var AccessTokenModel    = require('./libs/mongoose').AccessTokenModel;
+var RefreshTokenModel   = require('./libs/mongoose').RefreshTokenModel;
+var faker               = require('faker');
+
+UserModel.remove({}, function(err) {
+    var user = new UserModel({ username: "bat", password: "21091091" });
+    user.save(function(err, user) {
+        if(err) return log.error(err);
+        else log.info("New user - %s:%s",user.username,user.password);
+    });
+
+    for(i=0; i<4; i++) {
+        var user = new UserModel({ username: faker.name.firstName().toLowerCase(), password: "21091091" });
+        user.save(function(err, user) {
+            if(err) return log.error(err);
+            else log.info("New user - %s:%s",user.username,user.password);
+        });
+    }
+});
+
+ClientModel.remove({}, function(err) {
+    var client = new ClientModel({ name: "OurService Angular client v1", clientId: "angularV1", clientSecret:"21091091" });
+    client.save(function(err, client) {
+        if(err) return log.error(err);
+        else log.info("New client - %s:%s",client.clientId,client.clientSecret);
+    });
+});
+AccessTokenModel.remove({}, function (err) {
+    if (err) return log.error(err);
+});
+RefreshTokenModel.remove({}, function (err) {
+    if (err) return log.error(err);
+});
+
+setTimeout(function() {
+    mongoose.disconnect();
+}, 3000);
